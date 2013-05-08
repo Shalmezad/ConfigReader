52e45baeffa8e439f15d3132eb6cdadbacf7378f
Richard Wardin <shalmezad@gmail.com>
2013-05-08 15:06:33 -0400

    Added documentation

--- include/ConfigReader.h
+++ include/ConfigReader.h
@@ -4,19 +4,38 @@
 #include <vector>
 #include <string>
 
+/** \brief A Configuration Pair
+ * Storage for configuration name and value
+ */
 struct configuration
 {
+    /**
+     *The name of the configuration variable
+     */
     std::string configName;
+    /**
+     *The value of the configuration variable
+     */
     std::string configValue;
 };
 
+/** \brief A Configuration Reader
+ * Reads/parses configuration files */
 class ConfigReader
 {
     public:
+        /** Constructor*/
         ConfigReader();
+        /** \brief Reads a configuration file
+         * \param filename The name of the file to read
+         * \return True if file was read successfully*/
         bool readValues(std::string);
+        /** \brief Returns the configuration value
+         * \param varName Name of the configuration variable
+         * \return Value of the configuration variable*/
         std::string getConfig(std::string);
     private:
+        /** Storage for configuration pairs*/
         std::vector<configuration> configs;
 };
 
